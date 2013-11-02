793b933f73d6b5f3c896d369f43ab3b0027769f0
Alf Tang <tang.alf@gmail.com>
2013-11-02 15:41:30 +0800

    Add all listed Octave files

---
+++ IndexToAssignment.m
@@ -0,0 +1,22 @@
+% IndexToAssignment Convert index to variable assignment.
+%
+%   A = IndexToAssignment(I, D) converts an index, I, into the .val vector
+%   into an assignment over variables with cardinality D. If I is a vector, 
+%   then the function produces a matrix of assignments, one assignment 
+%   per row.
+%
+%   See also AssignmentToIndex.m and FactorTutorial.m
+
+function A = IndexToAssignment(I, D)
+
+D = D(:)'; % ensure that D is a row vector
+
+%cumprod([1, D(1:end - 1)]) produces 1 x D matrix
+%I(:) produces I x 1 matrix
+%repmat(I(:) - 1, 1, length(D)) produces I x D matrix
+%repmat(cumprod([1, D(1:end - 1)]), length(I), 1) produces I x D matrix
+
+A = mod(floor(repmat(I(:) - 1, 1, length(D)) ./ repmat(cumprod([1, D(1:end - 1)]), length(I), 1)), ...
+        repmat(D, length(I), 1)) + 1;
+  
+end
